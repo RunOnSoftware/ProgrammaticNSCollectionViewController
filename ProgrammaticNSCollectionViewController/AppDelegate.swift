ca50a7d0f7ce9c5cb4d71e9f7058a1439fe3e0b9
RunOnSoftware <johnmcknight@yahoo.com>
2014-12-21 16:17:54 -0500

    Initial Commit

---
+++ ProgrammaticNSCollectionViewController/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ProgrammaticNSCollectionViewController
+//
+//  Created by John McKnight on 12/21/14.
+//  Copyright (c) 2014 John McKnight. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
